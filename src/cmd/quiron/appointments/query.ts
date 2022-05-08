c74250a52b479934f2a74a60a862d2852ce5c346
juanmesa2097 <juanmesa2097@gmail.com>
2022-05-08 12:42:22 -0500

    Update appointments after uploading them

--- src/cmd/quiron/appointments/query.ts
+++ src/cmd/quiron/appointments/query.ts
@@ -1,17 +1,15 @@
-import { getConnection } from "../../../database/connection";
-import { readGlobalConfig } from "../../../utils/config";
-import { HttpService } from "../../../utils/http-service";
-import {
-  checkHttpStatus,
-  HTTPResponseError,
-} from "../../../utils/http-utilities";
-import { Logger } from "../../../utils/logger";
+import chalk from 'chalk';
+import { getConnection } from '../../../database/connection';
+import { readGlobalConfig } from '../../../utils/config';
+import { HttpService } from '../../../utils/http-service';
+import { checkHttpStatus, HTTPResponseError } from '../../../utils/http-utilities';
+import { Logger } from '../../../utils/logger';
 
 /**
  * List appointments from ChatbotCitas table
  * @param options command options
  */
-const listAppointments = async (options: { format: "json" | "table" }) => {
+const listAppointments = async (options: { format: 'json' | 'table' }) => {
   try {
     const pool = await getConnection();
     const query = await pool.request().query(`exec ObtenerChatBotCitas`);
@@ -19,12 +17,12 @@ const listAppointments = async (options: { format: "json" | "table" }) => {
 
     // Validate if there are any appointments to upload
     if (appointments.length === 0) {
-      Logger.log("No appointments were found");
+      Logger.log('No appointments were found');
       process.exit(0);
     }
 
     // Validate display format
-    if (options.format === "table") {
+    if (options.format === 'table') {
       Logger.table(appointments);
     } else {
       Logger.json(appointments);
@@ -53,11 +51,11 @@ const uploadAppointments = async () => {
   const { client, webhooks } = quironConfig || {};
 
   if (!client) {
-    Logger.error("Client config was not found");
+    Logger.error('Client config was not found');
   }
 
   if (!webhooks) {
-    Logger.error("Webhook config was not found");
+    Logger.error('Webhook config was not found');
   }
 
   try {
@@ -69,9 +67,7 @@ const uploadAppointments = async () => {
 
     // Validate if there are any appointments to upload
     if (appointments.length === 0) {
-      Logger.warn(
-        "No appointments were found. Make sure to prepare them first."
-      );
+      Logger.warn('No appointments were found. Make sure to prepare them first.');
       process.exit(0);
     }
 
@@ -82,14 +78,42 @@ const uploadAppointments = async () => {
     };
 
     // Call HTTP service to upload appointments
-    const response = await HttpService.post(
-      webhooks!.uploadAppointments!,
-      payload
-    );
+    const response = await HttpService.post(webhooks!.uploadAppointments!, payload);
 
     checkHttpStatus(response);
 
-    Logger.json(await response.json());
+    // Update appointments status
+    const {
+      isSuccess,
+      result: { responseSend },
+    } = await response.json();
+
+    if (isSuccess) {
+      for (const a of responseSend.result) {
+        var stmt = `
+          UPDATE ChatBotCitas
+          SET Enviado = $1, FechaHoraEnvio = '$2'
+          WHERE NumeroCita = $3 AND IdPaciente = $4`
+          .replace('$1', Boolean(a.enviado) ? '1' : '0')
+          .replace('$2', a.fechaHoraEnvio.slice(0, 19).replace('T', ' '))
+          .replace('$3', a.numeroCita)
+          .replace('$4', a.idPaciente);
+
+        const query = await pool.query(stmt);
+
+        Logger.log(
+          'Appointment:',
+          a.numeroCita,
+
+          'Pacient:',
+          `${a.nombresPaciente} (${a.celular})`,
+
+          '->',
+
+          query.rowsAffected[0] > 0 ? chalk.green.bold('SUCCEEDED') : chalk.red.bold('FAILED'),
+        );
+      }
+    }
   } catch (error) {
     if (error instanceof HTTPResponseError) {
       Logger.error(await error.response.text());
